03fcf4012bf88ad343cb52e678836039dd5397f7
Mukund Solanki <mukundsolanki665@gmail.com>
2023-08-01 22:58:49 +0530

    Configured the tailwind css for the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-green-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="btn">BUTTON</button>
