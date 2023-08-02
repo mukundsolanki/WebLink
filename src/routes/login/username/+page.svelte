ca43e8f15e44522e9b2432888c832037e1bc3bce
Mukund Solanki <mukundsolanki665@gmail.com>
2023-08-02 14:07:03 +0530

    Auth check for usenames

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
   import AuthState from "$lib/components/AuthState.svelte";
-  // import AuthState from "$lib/components/AuthState.svelte";
-  import { db, user } from "$lib/firebase";
+  import { db, user, userData } from "$lib/firebase";
   import { doc, getDoc, writeBatch } from "firebase/firestore";
-
   let username = "";
   let loading = false;
   let isAvailable = false;
-
   let debounceTimer: NodeJS.Timeout;
 
-  async function checkAvailability() {
+  const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+  $: isValid =
+    username?.length > 2 && username.length < 16 && re.test(username);
+  $: isTouched = username.length > 0;
+  $: isTaken = isValid && !isAvailable && !loading;
+
+  function checkAvailability() {
     isAvailable = false;
     clearTimeout(debounceTimer);
+    if (!isValid) {
+      loading = false;
+      return;
+    }
 
     loading = true;
 
@@ -28,23 +36,72 @@
   }
 
   async function confirmUsername() {
-    // TODO
+    console.log("confirming username", username);
+    const batch = writeBatch(db);
+    batch.set(doc(db, "usernames", username), { uid: $user?.uid });
+    batch.set(doc(db, "users", $user!.uid), {
+      username,
+      photoURL: $user?.photoURL ?? null,
+      published: true,
+      bio: "test bio.....",
+      links: [
+        {
+          title: "Test Link",
+          url: "https://github.com",
+          icon: "custom",
+        },
+      ],
+    });
+
+    await batch.commit();
+
+    username = "";
+    isAvailable = false;
   }
 </script>
 
 <AuthState>
-  <h2>Username</h2>
-  <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
-    <input
-      type="text"
-      placeholder="Username"
-      class="input w-full"
-      bind:value={username}
-      on:input={checkAvailability}
-    />
+  {#if $userData?.username}
+    <p class="text-lg">
+      Your username is <span class="text-success font-bold"
+        >@{$userData.username}</span
+      >
+    </p>
+    <p class="text-sm">(Usernames cannot be changed)</p>
+    <a class="btn btn-primary" href="/login/photo">Upload Profile Image</a>
+  {:else}
+    <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+      <input
+        type="text"
+        placeholder="Username"
+        class="input w-full"
+        bind:value={username}
+        on:input={checkAvailability}
+        class:input-error={!isValid && isTouched}
+        class:input-warning={isTaken}
+        class:input-success={isAvailable && isValid && !loading}
+      />
+      <div class="my-4 min-h-16 px-8 w-full">
+        {#if loading}
+          <p class="text-secondary">Checking availability of @{username}...</p>
+        {/if}
 
-    <p>Is available? {isAvailable}</p>
+        {#if !isValid && isTouched}
+          <p class="text-error text-sm">
+            must be 3-16 characters long, alphanumeric only
+          </p>
+        {/if}
 
-    <button class="btn btn-success">Confirm username @{username} </button>
-  </form>
-</AuthState>
+        {#if isValid && !isAvailable && !loading}
+          <p class="text-warning text-sm">
+            @{username} is not available
+          </p>
+        {/if}
+
+        {#if isAvailable}
+          <button class="btn btn-success">Confirm username @{username} </button>
+        {/if}
+      </div>
+    </form>
+  {/if}
+  </AuthState>
