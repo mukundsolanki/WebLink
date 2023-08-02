a8e7285f5c19e3dfc757e6cadb23888b516d51d5
Mukund Solanki <mukundsolanki665@gmail.com>
2023-08-02 12:23:13 +0530

    User login state completed

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+    import { auth, user } from "$lib/firebase";
+
+    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+
+    async function signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        const user = await signInWithPopup(auth, provider);
+        console.log(user)
+    }
+
 </script>
+
+<h2>Login</h2>
+
+{#if $user}
+  <h2 class="card-title">Welcome, {$user.displayName}</h2>
+  <p class="text-center text-success">You are logged in</p>
+  <button class="btn btn-warning" on:click={() => signOut(auth)}>Sign out</button>
+{:else}
+  <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+{/if}
