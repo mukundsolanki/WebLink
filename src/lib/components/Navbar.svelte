16f285db14ddece1e4d5407f91979654785db237
Mukund Solanki <mukundsolanki665@gmail.com>
2023-08-04 16:05:38 +0530

    added the about and profile page routes

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,8 @@
 <script>
+// @ts-nocheck
     import { getAuth, signOut } from "firebase/auth";
-
+    import { userData } from "$lib/firebase";
+    
     async function handleSignOut() {
       const auth = getAuth();
       
@@ -15,9 +17,10 @@
   </script>
   
   <nav class="bg-gray p-1 flex items-center justify-between m-0">
-    <div class="text-white pl-7 font-bold text-lg">WEBLINK</div>
+    <div class="text-black pl-7 font-bold text-lg">WEBLINK</div>
     <div class="flex items-center space-x-4">
-      <a href="/about" class="text-white hover:text-gray-300">About</a>
+      <a href="/{$userData.username}/edit" class="text-black font-semibold hover:text-gray-300">Profile</a>
+      <a href="/about" class="text-black hover:text-gray-300 font-semibold">About</a>
       <button class="btn btn-outline btn-error block mx-auto my-4" on:click={handleSignOut}>
         Sign Out
       </button>
