6c4633c883b75b5c045492cd49f0930006d50b48
Mukund Solanki <mukundsolanki665@gmail.com>
2023-08-24 21:22:12 +0530

    Updated the design of Display Page

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,20 +1,15 @@
 <script lang="ts">
+  import Footer from "$lib/components/Footer.svelte";
   import UserLink from "$lib/components/UserLink.svelte";
     import type { PageData } from "./$types";
-    
     export let data: PageData;
   </script>
   
   <svelte:head>
-    <title>@{data.username} Links</title>
+    <title>WebLink -@{data.username}</title>
   </svelte:head>
   
-  
   <main class="prose text-center mx-auto mt-8">
-
-    <h1 class="text-5xl text-stone-950">
-      @{data.username}
-    </h1>
   
     <img
       src={data.photoURL ?? "/user.png"}
@@ -22,13 +17,19 @@
       width="256"
       class="mx-auto"
     />
+
+    <h2 class="text-4xl text-stone-950">
+      @{data.username}
+    </h2>
   
     <ul class="list-none">
       {#each data.links as item}
       <li class="my-2">
         <UserLink {...item} />
+        <!-- <div class="divider"></div>  -->
       </li>
       {/each}
     </ul>
-  
+    
+    
   </main>
