c0c741822ee329b991a927c8d0735df73c9fe364
Mukund Solanki <mukundsolanki665@gmail.com>
2023-08-24 21:20:27 +0530

    added the Footed component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let githubLink = "https://github.com/mukundsolanki";
+</script>
+
+<footer
+  class="footer footer-center p-4 bg-base-300 text-base-content absolute bottom-0 w-full"
+>
+  <div>
+    <p>
+      Made with 💖 by
+      <a
+        class="link link-error"
+        href={githubLink}
+        target="_blank"
+        rel="noopener noreferrer">Mukund Solanki</a
+      >
+    </p>
+  </div>
+</footer>
